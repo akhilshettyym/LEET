19f40f77571352bff47fb35552458f330d241af2
akhilshettyym <akhilshettym2003@gmail.com>
2024-12-08 14:08:05 +0530

    Subsets of SQL

--- Simple programs/Add two numbers/with_user_inp.py
+++ Simple programs/Add two numbers/with_user_inp.py
@@ -2,3 +2,10 @@ num1 = int(input("Enter the first number :"))
 num2 = int(input("Enter the second number :"))
 sum = num1 + num2
 print("Sum of the numbers is : ", sum)
+
+
+# For decimal numbers
+num1 = input("Enter the first number :")
+num2 = input("Enter the second number :")
+sum = float(num1) + float(num2)
+print(f"Sum of the numbers {num1} + {num2} is : ", sum)
