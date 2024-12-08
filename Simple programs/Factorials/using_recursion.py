242592239bfb6e2d0c186c1a817257558639326e
akhilshettyym <akhilshettym2003@gmail.com>
2024-12-08 20:11:39 +0530

    factorial using recursion

--- Simple programs/Factorials/using_recursion.py
+++ Simple programs/Factorials/using_recursion.py
@@ -1,1 +1,5 @@
 # Get Factorial of a Number using a Recursive Approach
+def factorial(num):
+    return 1 if (num==0 or num==1) else num * factorial (num-1)
+num = 4
+print(f"Factorial of the number {num} is {factorial(num)}")
