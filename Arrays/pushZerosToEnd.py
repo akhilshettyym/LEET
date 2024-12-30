fd3321649673d49dda756a3c67d3e302c4016f15
akhilshettyym <akhilshettym2003@gmail.com>
2024-12-30 20:36:00 +0530

    GFG Pushing zeros to the end of the array

---
+++ Arrays/pushZerosToEnd.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pushZerosToEnd(self, arr):
+        
+        non_zero_pos = 0
+        for i in range(len(arr)):
+            if arr[i] != 0:
+                arr[non_zero_pos] = arr[i]
+                non_zero_pos += 1
+        while non_zero_pos < len(arr):
+            arr[non_zero_pos] = 0
+            non_zero_pos += 1
+        return arr
+    
+arr = [3, 5, 0, 0, 4]
+sol = Solution()
+result = sol.pushZerosToEnd(arr)
+print(result)
